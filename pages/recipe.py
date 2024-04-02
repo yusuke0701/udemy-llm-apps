a95d3e5284aa6d63c58664c8f0850d7ae046e58e
中島 悠輔 <nakashima.yusuke@headwaters.co.jp>
2024-04-02 06:59:49 +0000

    4.4章

--- pages/recipe.py
+++ pages/recipe.py
@@ -1,15 +1,30 @@
 import streamlit as st
 import json
 from openai import OpenAI
+from pydantic import BaseModel, Field
 
 
-f = open("static/json/recipe.json", "r", encoding="UTF-8")
-SAMPLE_JSON = f.read()
-f.close()
+class Ingredient(BaseModel):
+    ingredient: str = Field(description="材料", examples=["鶏肉"])
+    quantity: str = Field(description="分量", examples=["300g"])
+
+
+class Recipe(BaseModel):
+    ingredients: list[Ingredient]
+    instructions: list[str] = Field(
+        description="手順", examples=[["材料を切ります。", "材料を炒めます。"]]
+    )
+
 
 f = open("static/prompt/recipe.txt", "r", encoding="UTF-8")
 PROMPT_TEMPLATE = f.read()
 f.close()
+
+OUTPUT_RECIPE_FUNCTION = {
+    "name": "output_recipe",
+    "description": "レシピを出力する",
+    "parameters": Recipe.schema(),
+}
 
 st.title("レシピ生成AI")
 
@@ -19,31 +34,38 @@ client = OpenAI()
 
 if dish:
     with st.spinner(text="生成中..."):
+        messages = [
+            {
+                "role": "user",
+                "content": PROMPT_TEMPLATE.format(dish=dish),
+            }
+        ]
+        tools = [{"type": "function", "function": OUTPUT_RECIPE_FUNCTION}]
+
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "user",
-                    "content": PROMPT_TEMPLATE.format(
-                        sample_json=SAMPLE_JSON, dish=dish
-                    ),
-                }
-            ],
+            messages=messages,
+            tools=tools,
+            tool_choice="auto",
         )
-        st.write(response)
 
-        recipe = json.loads(SAMPLE_JSON)
+        response_message = response.choices[0].message
+        tool_calls = response_message.tool_calls
 
-        st.write("## 材料")
-        st.table(recipe["ingredients"])
+        if tool_calls:
+            for tool_call in tool_calls:
+                recipe = json.loads(tool_call.function.arguments)
 
-        # 以下のマークダウンの文字列を作成して表示する
-        #
-        # ## 手順
-        # 1. 材料を切ります。
-        # 2. 材料を炒めます。
+                st.write("## 材料")
+                st.table(recipe["ingredients"])
 
-        instruction_markdown = "## 手順\n"
-        for i, instruction in enumerate(recipe["instructions"]):
-            instruction_markdown += f"{i+1}. {instruction}\n"
-        st.write(instruction_markdown)
+                # 以下のマークダウンの文字列を作成して表示する
+                #
+                # ## 手順
+                # 1. 材料を切ります。
+                # 2. 材料を炒めます。
+
+                instruction_markdown = "## 手順\n"
+                for i, instruction in enumerate(recipe["instructions"]):
+                    instruction_markdown += f"{i+1}. {instruction}\n"
+                st.write(instruction_markdown)
