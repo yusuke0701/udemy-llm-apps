8ee35e690686a779928344861d141fedce8d78a7
中島 悠輔 <nakashima.yusuke@headwaters.co.jp>
2024-04-02 09:15:24 +0000

    6章 完了

--- pages/qa.py
+++ pages/qa.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import tempfile
+from pathlib import Path
+from langchain.chat_models import ChatOpenAI
+from llama_index.core import VectorStoreIndex
+from llama_index.core.callbacks import CallbackManager
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.readers.file.docs import PDFReader
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 st.title("PDFへのQ&A")
 
@@ -19,9 +28,14 @@ if uploaded_file and index is None:
             f.write(uploaded_file.getbuffer())
             st.write(f"ファイル名: {f.name}")
 
-            # TODO: 文章をベクトル化して、Q&Aの準備を実施
+            documents = PDFReader().load_data(file=Path(f.name))
 
-            index = "index"
+            # ドキュメントをベクトル化して、ベクトルストアに保存する
+            embed_model = OpenAIEmbedding()
+            callback_manager = CallbackManager()
+            index = VectorStoreIndex.from_documents(
+                documents, embed_model=embed_model, callback_manager=callback_manager
+            )
             st.session_state["index"] = index
 
 if index is not None:
@@ -29,5 +43,8 @@ if index is not None:
 
     if question:
         with st.spinner(text="考え中..."):
-            answer = "回答"
-            st.write(answer)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            query_engine = index.as_query_engine(llm=llm)
+            answer = query_engine.query(question)
+            st.write(answer.response)
+            st.info(answer.source_nodes)
