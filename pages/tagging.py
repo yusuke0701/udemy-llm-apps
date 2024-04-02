7ac7f5b21f502831c08e4223b8559a414f3759c8
中島 悠輔 <nakashima.yusuke@headwaters.co.jp>
2024-04-02 08:00:16 +0000

    5章 完了

--- pages/tagging.py
+++ pages/tagging.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from pydantic import BaseModel, Field
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import create_tagging_chain_pydantic
+
+
+class Attribute(BaseModel):
+    language: str = Field(enum=["ja", "en"])
+    tags: list[str] = Field(examples=[["Python", "Streamlit"]])
+
 
 st.title("タグ付け")
 
@@ -6,5 +15,7 @@ text = st.text_area(label="タグ付けするテキスト")
 
 if text:
     with st.spinner("タグ付け中..."):
-        attr = {"language": "ja", "tags": ["Python", "Streamlit"]}
-        st.write(attr)
+        llm = ChatOpenAI(model="gpt-3.5-turbo")
+        chain = create_tagging_chain_pydantic(Attribute, llm)
+        attr = chain.run(text)
+        st.write(attr.dict())
